6cf3e14d26e57a55259a7e69e6434b9e0bef968a
geilson <geilsonsrz@gmail.com>
2024-12-26 23:18:40 -0300

    Criando SSH

--- src/componentes/Menu/index.jsx
+++ src/componentes/Menu/index.jsx
@@ -15,16 +15,7 @@ import './Menu.scss'
 export default function Menu() {
 
     return (
-        <div className="menu">
-
-            <span >Item 1</span>
-
-            <span >Item 2</span>
-
-            <span >Item 3</span>
-
-            <span >Item 4</span>
-
-        </div>
+        <>
+        </>
     )
 }
