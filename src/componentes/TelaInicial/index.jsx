02d748a03c732edb63cfaa36f126e2e85d49c367
geilson <geilsonsrz@gmail.com>
2024-12-28 21:13:09 -0300

    Estitulo tela 1

--- src/componentes/TelaInicial/index.jsx
+++ src/componentes/TelaInicial/index.jsx
@@ -6,11 +6,16 @@ export default function TelaInicial({className}) {
 
     return (
         <span id='telaInicial' className={`tela telaInicial bordaGradiente ${className || ''}`}>
-            <span className='bgFoto'>
-                <span className='txtTitulo fontePrimaria'>
-                    Olá
+            <span className='conteinerTelaInicial'>
+                <span className='fraseTitulo'>
+                    "Alguma frase de filosofía de trabalho aqui." - Jordanio
                 </span>
-                <Botao className='btn fonteSecundaria'/>
+
+                <span className='cardTitulo'>
+                    <>Prof. Jó</>
+                    <>Botão</>
+                </span>
+
             </span>
         </span>
     )
